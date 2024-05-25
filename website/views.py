247d2a66c01638468943d9441e416ccd8d684947
Marcus Guiban <guiban.marcus@gmail.com>
2024-05-26 01:16:42 +0800

    final commit

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .froms import SignUpForm
+from .froms import SignUpForm, AddRecordForm
 from .models import Record
 
 def home(request):
@@ -66,3 +66,29 @@ def delete_record(request, pk):
     else:
         messages.success(request, "You Must be logged in to delete this record")
         return redirect('home')
+    
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+            if request.method == "POST":
+                if form.is_valid():
+                    add_record = form.save()
+                    messages.success(request, "Record Added")
+                    return redirect('home')
+            return render(request, 'add_record.html',{'form':form})
+    else:
+        messages.success(request, "You Must be logged in to add record")
+        return redirect('home')
+    
+def update_record(request, pk):
+	if request.user.is_authenticated:
+		current_record = Record.objects.get(id=pk)
+		form = AddRecordForm(request.POST or None, instance=current_record)
+		if form.is_valid():
+			form.save()
+			messages.success(request, "Record Has Been Updated!")
+			return redirect('home')
+		return render(request, 'update_record.html', {'form':form})
+	else:
+		messages.success(request, "You Must Be Logged In...")
+		return redirect('home')
